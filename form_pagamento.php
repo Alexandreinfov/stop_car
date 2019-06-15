7b543392f2a0df067b82ccd0b82da449774a7a95
Alexandreinfov <alexandrevalter88@gmail.com>
2019-06-15 18:42:55 -0300

    15/06/219 18:50

--- form_pagamento.php
+++ form_pagamento.php
@@ -1,41 +1,20 @@
 <?php
-<<<<<<< HEAD
-	
-=======
+
 	include("classeCabecalho.php");
 	$c->exibe_menu();
 	include("classeTabela.php");
 	include("classeBancoDeDados.php");
 
->>>>>>> 50f5c5dc0beb8004fb6449da1816173a4391061b
 	include("classeForm.php");
 	include("conexao.php");
 	include("autenticacao.php");
 
-<<<<<<< HEAD
 	$parametros = null;
-	$parametros["action"] = "insere.php?tabela=";
-=======
-		$parametros = null;
 	$parametros["action"] = "insere.php?tabela=tipo_pagamento";
->>>>>>> 50f5c5dc0beb8004fb6449da1816173a4391061b
 	$parametros["method"] = "post";
 	$parametros["tipoForm"] = "Ordem de Serviço";
 	$f = new Form($parametros);
 
-<<<<<<< HEAD
-	//SELECT PAGAMENTO
-	$consulta = "SELECT COD_PAGAMENTO as value, NOME as label FROM TIPO_PAGAMENTO";
-	$stmt = $conexao->prepare($consulta);
-	$stmt->execute();
-	$funcionarios = null;
-	while($linha = $stmt->fetch()){ //pegando os valores linha por linha.
-		$funcionarios[] = $linha;
-	}
-	$parametros["name"] = "COD_FUNCIONARIO";
-	$parametros["label"] = "Pagamento";
-	$p->add_pagamento($parametros, $funcionarios, null); 
-=======
 	$parametros = null;
 	$parametros["name"] = "DESCRICAO_TIPO";
 	$parametros["type"] = "radio";
@@ -49,8 +28,6 @@
 	$parametros["name"] = "PARCELAS";
 	$parametros["label"] = "Parcelas";
 	$f->add_select($parametros, $parcela, null);
->>>>>>> 50f5c5dc0beb8004fb6449da1816173a4391061b
-
 
 	$parametros = "Cadastrar";
 	$f->add_button($parametros);
