bc817f16a45fb635019dea40d8b058d0d14eb080
Alexandreinfov <alexandrevalter88@gmail.com>
2019-06-15 14:14:33 -0300

    banco

--- os.sql
+++ os.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 06-Jun-2019 às 00:29
+-- Data de Criação: 15-Jun-2019 às 17:13
 -- Versão do servidor: 5.6.13
 -- versão do PHP: 5.4.17
 
@@ -36,7 +36,15 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `TELEFONE` varchar(20) NOT NULL,
   PRIMARY KEY (`COD_CLIENTE`),
   UNIQUE KEY `CPF` (`CPF`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`COD_CLIENTE`, `NOME`, `CPF`, `ENDERECO`, `TELEFONE`) VALUES
+(1, 'Filipe', 2147483647, '465sd45df64sdf', '445212525'),
+(2, 'adas', 548575, 'fgfdzg', '75741');
 
 -- --------------------------------------------------------
 
@@ -53,7 +61,14 @@ CREATE TABLE IF NOT EXISTS `funcionario` (
   `SALARIO` decimal(20,0) NOT NULL,
   PRIMARY KEY (`COD_FUNCIONARIO`),
   UNIQUE KEY `CPF` (`CPF`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `funcionario`
+--
+
+INSERT INTO `funcionario` (`COD_FUNCIONARIO`, `NOME`, `CPF`, `ENDERECO`, `TELEFONE`, `SALARIO`) VALUES
+(1, 'asdre', 546, 'nkinj', '9926486165', '53436');
 
 -- --------------------------------------------------------
 
@@ -65,6 +80,7 @@ CREATE TABLE IF NOT EXISTS `ordem_servico` (
   `COD_OS` int(11) NOT NULL AUTO_INCREMENT,
   `DATA_ABERTURA` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `DATA_FECHAMENTO` datetime DEFAULT NULL,
+  `COD_SERVICO_PRODUTO` int(6) NOT NULL,
   `COD_FUNCIONARIO` int(11) NOT NULL,
   `COD_CLIENTE` int(11) NOT NULL,
   `COD_VEICULO` int(11) NOT NULL,
@@ -73,7 +89,15 @@ CREATE TABLE IF NOT EXISTS `ordem_servico` (
   KEY `COD_FUNCIONARIO` (`COD_FUNCIONARIO`),
   KEY `COD_CLIENTE` (`COD_CLIENTE`),
   KEY `COD_VEICULO` (`COD_VEICULO`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `ordem_servico`
+--
+
+INSERT INTO `ordem_servico` (`COD_OS`, `DATA_ABERTURA`, `DATA_FECHAMENTO`, `COD_SERVICO_PRODUTO`, `COD_FUNCIONARIO`, `COD_CLIENTE`, `COD_VEICULO`, `STATUS`) VALUES
+(1, '2019-06-12 20:13:24', NULL, 3, 1, 1, 5, 'aberto'),
+(2, '2019-06-12 21:31:10', NULL, 10, 1, 1, 7, 'aberto');
 
 -- --------------------------------------------------------
 
@@ -98,11 +122,35 @@ CREATE TABLE IF NOT EXISTS `pagamento` (
 
 CREATE TABLE IF NOT EXISTS `servico_produto` (
   `COD_SERVICO_PRODUTO` int(11) NOT NULL AUTO_INCREMENT,
-  `TIPO` enum('servico','produto') NOT NULL,
+  `TIPO` enum('p','s') NOT NULL,
   `DESCRICAO` varchar(50) NOT NULL,
   `PRECO` decimal(8,2) NOT NULL,
   PRIMARY KEY (`COD_SERVICO_PRODUTO`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+--
+-- Extraindo dados da tabela `servico_produto`
+--
+
+INSERT INTO `servico_produto` (`COD_SERVICO_PRODUTO`, `TIPO`, `DESCRICAO`, `PRECO`) VALUES
+(1, '', 'asdds', '10.00'),
+(2, 'p', 'asdds', '10.00'),
+(3, 's', 'hbhj', '15247.00'),
+(4, 's', 'hbhj', '15247.00'),
+(5, 's', 'hbhj', '15247.00'),
+(6, 's', 'hbhj', '15247.00'),
+(7, 's', 'hbhj', '15247.00'),
+(8, 's', 'OLA', '1.00'),
+(9, 's', 'OLA', '1.00'),
+(10, 'p', 'OLAaaa', '54.00'),
+(11, 'p', 'zx', '0.00'),
+(12, 's', 'zx', '0.00'),
+(13, 'p', 'zx', '0.00'),
+(14, 'p', 'zx', '0.00'),
+(15, 'p', 'zx', '0.00'),
+(16, 'p', 'zx', '0.00'),
+(17, 'p', 'zx', '0.00'),
+(18, 'p', 'zx', '0.00');
 
 -- --------------------------------------------------------
 
@@ -156,7 +204,18 @@ CREATE TABLE IF NOT EXISTS `veiculo` (
   PRIMARY KEY (`COD_VEICULO`),
   UNIQUE KEY `PLACA` (`PLACA`),
   KEY `COD_CLIENTE` (`COD_CLIENTE`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+
+--
+-- Extraindo dados da tabela `veiculo`
+--
+
+INSERT INTO `veiculo` (`COD_VEICULO`, `PLACA`, `MODELO`, `MARCA`, `ANO`, `COD_CLIENTE`) VALUES
+(5, 'ale1506', 'ford', 'fusca', 1951, 1),
+(7, 'ale758', 'ford', 'fusca', 1951, 1),
+(17, 'as5844', 'ASD', 'SAD', 1968, 1),
+(18, 'ABP0987', 'HILUX', 'TOYOTA', 2019, 1),
+(19, 'ATT0101', 'TORO', 'FIAT', 2018, 2);
 
 -- --------------------------------------------------------
 
