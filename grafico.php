303c121efc9bd6deac0b5becdebb9898f89469f0
Alexandreinfov <alexandrevalter88@gmail.com>
2019-06-24 01:25:05 -0300

    STOP_CAR 24/06/2019 01:25

--- grafico.php
+++ grafico.php
@@ -1,50 +1,74 @@
 <?php
-		include 'phplot-6.2.0/phplot.php';
+	//manual dos tipos de graficos:
+	//http://phplot.sourceforge.net/phplotdocs/
+	include 'phplot-6.2.0/phplot.php';
 
-  	include("conexao.php");
+	include("conexao.php");
 
-   	$consulta = 'SELECT COUNT( 
-STATUS ) 
-FROM os
-WHERE STATUS =  "orçamento"';
+	$consulta = 'SELECT COUNT( 
+	STATUS ) 
+	FROM os
+	WHERE STATUS =  "orçamento"';
 	$stmt = $conexao->prepare($consulta);
 	$stmt->execute();
 	$orcamento = null;
 	$orcamento = $stmt->fetch();
-	//while($linha = $stmt->fetch()){ //pegando os valores linha por linha.
-	//	$status[] = $linha;
-	//}
-	//mysql_close($conexao);
 
-  	$consulta = 'SELECT COUNT( 
-STATUS ) 
-FROM os
-WHERE STATUS =  "aprovado"';
+
+	$consulta = 'SELECT COUNT( 
+	STATUS ) 
+	FROM os
+	WHERE STATUS =  "aprovado"';
 	$stmt = $conexao->prepare($consulta);
 	$stmt->execute();
 	$aprovado = null;
 	$aprovado = $stmt->fetch();
 
 
-  	$consulta = 'SELECT COUNT( 
-STATUS ) 
-FROM os
-WHERE STATUS =  "finalizado"';
+	$consulta = 'SELECT COUNT( 
+	STATUS ) 
+	FROM os
+	WHERE STATUS =  "finalizado"';
 	$stmt = $conexao->prepare($consulta);
 	$stmt->execute();
 	$finalizado = null;
 	$finalizado = $stmt->fetch();
 
 
-  	$consulta = 'SELECT COUNT( 
-STATUS ) 
-FROM os
-WHERE STATUS =  "faturada"';
+	$consulta = 'SELECT COUNT( 
+	STATUS ) 
+	FROM os
+	WHERE STATUS =  "faturado"';
 	$stmt = $conexao->prepare($consulta);
 	$stmt->execute();
-	$faturada = null;
-	$faturada = $stmt->fetch();
-	
+	$faturado = null;
+	$faturado = $stmt->fetch();
+
+	$title = 'Andamentos das ordens de servico';
+	$data = array(
+		array('orcamento' , $orcamento[0] ), 
+		array('aprovado' , $aprovado[0] ),
+		array('finalizado' , $finalizado[0] ),
+		array('faturado' , $faturado[0] )   
+	);     
+	function mycallback($str)
+	{
+		list($percent, $label) = explode(' ', $str, 2);
+		return sprintf('%s (%.1f%%)', $label, $percent);
+	}
+
+	$plot = new PHPlot(500, 350);
+	$plot->SetImageBorderType('plain'); // Improves presentation in the manual
+	$plot->SetPlotType('pie');
+	$plot->SetDataType('text-data-single');
+	$plot->SetDataValues($data);
+	$plot->SetTitle($title);
+	# Set label type: combine 2 fields and pass to custom formatting function
+	$plot->SetPieLabelType(array('percent', 'label'), 'custom', 'mycallback');
+	$plot->DrawGraph();
+
+
+
   	//echo "<pre>";
 //  	echo $funcionarios[0]["STATUS"];
   	//print_r($funcionarios);
@@ -95,26 +119,5 @@ $plot->DrawGraph();
 
 */
 
-$title = 'Andamentos das ordens de servico';
-$data = array(
-             array('orcamento' , $orcamento[0] ), 
-             array('aprovado' , $aprovado[0] ),
-             array('finalizado' , $finalizado[0] ),
-             array('faturado' , $faturada[0] )   
-             );     
-function mycallback($str)
-{
-    list($percent, $label) = explode(' ', $str, 2);
-    return sprintf('%s (%.1f%%)', $label, $percent);
-}
 
-$plot = new PHPlot(500, 350);
-$plot->SetImageBorderType('plain'); // Improves presentation in the manual
-$plot->SetPlotType('pie');
-$plot->SetDataType('text-data-single');
-$plot->SetDataValues($data);
-$plot->SetTitle($title);
-# Set label type: combine 2 fields and pass to custom formatting function
-$plot->SetPieLabelType(array('percent', 'label'), 'custom', 'mycallback');
-$plot->DrawGraph();
 ?>
